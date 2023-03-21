53d08fca87b7a73eb7505137f52e4f93f437f401
Ítalo Iran <italow6@gmail.com>
2023-03-21 08:27:38 -0300

    Pequenos ajustes

--- index.js
+++ index.js
@@ -2,8 +2,34 @@ const url = 'https://itinhuz.vercel.app/server/pokedex.json'
 const axios = require('axios')
 
 //Esse endpoint exibe todos os pokemons
-axios.get(url).then(response => {
+function exibirTodos(){
+    axios.get(url).then(response => {
     const pokemons = response.data
     
     console.log(pokemons)
 })
+}
+
+//Esse endpoint exibe todos os pokemons tipo 'Eletric'
+function exibeEletricos(){
+    axios.get(url).then(response => {
+        const pokemons = response.data
+        const poke = pokemons.filter(eletrico)    
+        console.log(poke)
+    })
+}
+//Endpoint exibe todos os pokemons que tem velocidade >= 40
+
+function velocidade40mais(velocidade){
+    const speed40 = f => f.base.Speed >= velocidade
+    
+    axios.get(url).then(response => {
+        const pokemons = response.data
+
+    const poke = pokemons.filter(speed40)
+    console.log(poke);
+    })    
+}
+
+velocidade40mais(150) 
+
